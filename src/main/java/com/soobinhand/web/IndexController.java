887861faba8c130b177b8687f673d46a138a50e8
soobinhand <chelsea9528@naver.com>
2021-08-17 23:38:51 +0900

    a

--- src/main/java/com/soobinhand/web/IndexController.java
+++ src/main/java/com/soobinhand/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.soobinhand.web;
 
+import com.soobinhand.config.auth.LoginUser;
 import com.soobinhand.config.auth.dto.SessionUser;
 import com.soobinhand.service.*;
 
@@ -20,9 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user!=null){
             model.addAttribute("userName",user.getName());
         }
